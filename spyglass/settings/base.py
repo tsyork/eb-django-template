7f0bc6c6e0946072f3dd2c9812d04f15456510ba
tsyork <tsyork@gmail.com>
2018-05-19 17:48:03 -0700

    Update settings

--- spyglass/settings/base.py
+++ spyglass/settings/base.py
@@ -74,6 +74,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'spyglass.wsgi.application'
 
 
+
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
@@ -132,4 +133,33 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
+
+AWS_ACCESS_KEY_ID = "AKIAJSMNIBDAGIOL7JOA"
+AWS_SECRET_ACCESS_KEY = "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"
+
+
+AWS_FILE_EXPIRE = 200
+AWS_PRELOAD_METADATA = True
+AWS_QUERYSTRING_AUTH = True
+
+DEFAULT_FILE_STORAGE = 'spyglass.utils.MediaRootS3BotoStorage'
+STATICFILES_STORAGE = 'spyglass.utils.StaticRootS3BotoStorage'
+AWS_STORAGE_BUCKET_NAME = 'spyglass-app'
+S3DIRECT_REGION = 'us-west-3'
+S3_URL = '//%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+MEDIA_URL = '//%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
+MEDIA_ROOT = MEDIA_URL
+STATIC_URL = S3_URL + 'static/'
+ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+
+import datetime
+
+two_months = datetime.timedelta(days=61)
+date_two_months_later = datetime.date.today() + two_months
+expires = date_two_months_later.strftime("%A, %d %B %Y 20:00:00 GMT")
+
+AWS_HEADERS = {
+	'Expires': expires,
+	'Cache-Control': 'max-age=%d' % (int(two_months.total_seconds()), ),
+}
